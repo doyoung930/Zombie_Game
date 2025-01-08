0ba6659d403686be5bac2eae35eedd80ea9c02f9
doyoung930 <doyoung930@gmail.com>
2025-01-08 16:26:20 +0900

    Effect 추가

--- Zombie_game/Assets/Scripts/Weapon.cs
+++ Zombie_game/Assets/Scripts/Weapon.cs
@@ -9,6 +9,16 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
     
+    [SerializeField] GameObject hitEffect;
+    // muzzle
+    //[SerializeField] ParticleSystem muzzleFlash;
+    public GameObject muzzlePrefab;
+    public GameObject muzzlePosition;
+    public GameObject projectilePrefab;
+    public GameObject projectileToDisableOnFire;
+    
+    
+    
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -19,20 +29,56 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+       
+    }
+
+    private void PlayMuzzleFlash()
+    {
+
+        // --- Spawn muzzle flash ---
+        var flash = Instantiate(muzzlePrefab, muzzlePosition.transform);
+
+        // --- Shoot Projectile Object ---
+        if (projectilePrefab != null)
+        {
+            GameObject newProjectile = Instantiate(projectilePrefab, muzzlePosition.transform.position, muzzlePosition.transform.rotation, transform);
+        }
+
+        // --- Disable any gameobjects, if needed ---
+        if (projectileToDisableOnFire != null)
+        {
+            projectileToDisableOnFire.SetActive(false);
+            Invoke("ReEnableDisabledProjectile", 3);
+        }
+            
+    }
+    private void ReEnableDisabledProjectile()
+    {
+        projectileToDisableOnFire.SetActive(true);
+    }
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
+            CreateHitImpact(hit);
+            
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
             target.TakeDamage(damage);
         }
         else
         {
-            return; 
+            return;
         }
-        
+    }
 
-        
-        
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
     }
 }
